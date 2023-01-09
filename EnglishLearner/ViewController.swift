c340499efc38b3eead34e96f7f870edbfa86ada2
Gleb Syomshchikov <g.semshchikov@corp.mail.ru>
2023-01-09 13:20:40 +0700

    Initial Commit

---
+++ EnglishLearner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnglishLearner
+//
+//  Created by g.semshchikov on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
